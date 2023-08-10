bfc15e8562a5c6639a6b619278ae0da104c5bc96
amineNouabi <aminenouabi9@gmail.com>
2023-08-10 08:29:45 -0700

    First c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ *  main - prints to the console
+ *
+ *  Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle");
+	return (0);
+}
