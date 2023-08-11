2dfc52bc89ca19733b1ddb31eea5341011aea559
amineNouabi <aminenouabi9@gmail.com>
2023-08-11 06:50:39 -0700

    Sign

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/*
+ * main - generates a random number and checks its sign
+ *
+ * Return : Always 0
+ *
+ */
+int main (void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	printf("%d is ", n);
+	if(n > 0)
+		printf("positive\n");
+	else if (n < 0)
+		printf("negative\n");
+	else
+		printf("zero\n");
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Control statements/flow and variables
