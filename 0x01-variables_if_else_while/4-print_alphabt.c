af56d6ee0d5f22eec62a20134771bdfd08276d56
amineNouabi <aminenouabi9@gmail.com>
2023-08-11 07:17:28 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ *	main - Prints lowercase alphabet and skips 'q' and 'e'
+ *
+ *	Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
