039195f99b6b7c62e84aa0afbc90bfdd2534658b
amineNouabi <aminenouabi9@gmail.com>
2023-08-16 03:22:54 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+/**
+ * print_sign - Prints '+' or '-' depending on parametre sign
+ * @n: positive or negative integer.
+ * Return: 1 if n positive | -1 if n negative | 0 if n is 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_puthar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
