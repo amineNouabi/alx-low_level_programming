91caa4afe0f4ddfc569db5f44ba1a7d9e9f31837
amineNouabi <aminenouabi9@gmail.com>
2023-08-16 15:00:59 -0700

    Added INT MIN edge case

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,7 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = n > 0 ? n % 10 : -n % 10;
+	last_digit = n > 0 ? n % 10 : -(long int)n % 10;
 
 	_putchar(last_digit + '0');
 	return (last_digit);
