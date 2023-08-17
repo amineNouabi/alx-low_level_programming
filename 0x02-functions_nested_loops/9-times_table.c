eb3d0949609118e89be7493744bb65bb12512cb9
amineNouabi <aminenouabi9@gmail.com>
2023-08-16 17:53:30 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * times_table - prints multiplication table by lines
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int number, factor, res;
+
+	number = 0;
+
+	while (number < 10)
+	{
+		factor = 0;
+
+		while (factor < 10)
+		{
+			res = number * factor;
+
+			if (factor == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (res > 9)
+				{
+					_putchar(res / 10 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+	
+				_putchar(res % 10 + '0');
+			}
+			factor++;
+		}
+		_putchar('\n');
+		number++;
+	}
+}
+
+				
+				
+
+
