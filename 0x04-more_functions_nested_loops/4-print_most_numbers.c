45d4512d9758abf051fb95d00abfafcd6d8beb58
amineNouabi <aminenouabi9@gmail.com>
2023-08-17 18:08:14 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - prints 0123456789 followed by '\n'
+ * print_most_numbers - prints 0123456789 followed by '\n'
  * Return: void
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char c = '0';
 
