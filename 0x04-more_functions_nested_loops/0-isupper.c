e6504c2ded11c1ff2ff5f77b8c6fe0b60b7824a1
amineNouabi <aminenouabi9@gmail.com>
2023-08-17 17:42:08 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _isupper - check if char is upper
+ * @c: integer representing character
+ * Return: 1 if upper, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z' ? 1 : 0);
