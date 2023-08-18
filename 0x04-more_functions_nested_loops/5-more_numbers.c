6964fa5ecc2db41147737180da5d746cee154cf6
amineNouabi <aminenouabi9@gmail.com>
2023-08-17 18:13:13 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times from 0 to 14.
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int num, n;
+
+	n = 10;
+	while (n--)
+	{
+		num = 0;
+		while (num < 15)
+		{
+			if (num > 0)
+				_putchar(num / 10 + '0');
+			_putchar(num % 10 + '0');
+			num++;
+		}
+	}
+}
