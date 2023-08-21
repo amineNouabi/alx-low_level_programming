89695d76204d52cff7ab00e2ca0a1ab457d39989
amineNouabi <aminenouabi9@gmail.com>
2023-08-21 11:42:25 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string.
+ * @str: String
+ * Return: void
+ */
+void puts2(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+
+		if (!(str + 1))
+			break;
+		str += 2;
+	}
+}
