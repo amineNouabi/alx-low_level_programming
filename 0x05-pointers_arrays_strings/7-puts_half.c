feb774a7c126f8a623d52cc4f6f5d4b6a348adf8
amineNouabi <aminenouabi9@gmail.com>
2023-08-21 12:29:47 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,10 @@ void puts_half(char *str)
 	while (str[length])
 		length++;
 
-	startingIndex = length / 2 + length % 2 != 0 ? 1 : 0;
+	startingIndex = length / 2;
+	if (length % 2 != 0)
+		startingIndex += 1;
+
 	for (i = startingIndex; i < length; i++)
 		_putchar(str[i]);
 	_putchar('\n');
