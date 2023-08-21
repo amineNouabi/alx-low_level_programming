b921ee7b3b1e4a99dffebb94a519a3634c9c7d69
amineNouabi <aminenouabi9@gmail.com>
2023-08-21 13:15:30 -0700

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,14 +8,14 @@ int _atoi(char *s)
 	int number = 0;
 	int sign = 1;
 
-	while (s)
+	while (*s)
 	{
 		if (*s == '-' && !number)
 			sign *= -1;
 		if (*s >= '0' && *s <= '9')
-			number = number * 10 + (*s + '0');
+			number = number * 10 + (*s - '0');
 		else
-			if (!number)
+			if (number)
 				break;
 		s++;
 	}
