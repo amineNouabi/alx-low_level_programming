0b0a29b618a3273b909522d2ee1f22756e2d2c8b
amineNouabi <aminenouabi9@gmail.com>
2023-08-23 10:50:58 -0700

    6

---
+++ 0x06-pointers_arryas_strings/6-cap_string.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * to_upper - turn lowercase char into uppercase
+ * @c: Character
+ * Return: void
+ */
+static void to_upper(char *c)
+{
+	if (*c >= 'a' && *c <= 'z')
+		*c -= 32;
+}
+
+/**
+* to_lower - turn uppercase char into lowercase
+* @c: Character
+* Return: void
+*/
+static void to_lower(char *c)
+{
+	if (*c >= 'A' && *c <= 'Z')
+		*c += 32;
+}
+
+/**
+ * is_separator - wraps separators
+ * @c: Character
+ * Return: 1 if is seperator and  0 otherwise.
+ */
+int	is_separator(char c)
+{
+	char sep[13] = ",;.!?\"(){} \t\n";
+	int i = 0;
+
+	while (sep[i])
+	{
+		if (c == sep[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
+ * cap_string - Capitalizes a string.
+ * @str: String:
+ * Return: String
+ */
+
+char *cap_string(char *str)
+{
+	unsigned int	i;
+	int				word;
+
+	i = 0;
+	word = 1;
+	while (str[i])
+	{
+		if (word == 1)
+			to_upper(&str[i]);
+		else
+			to_lower(&str[i]);
+
+		if (!is_separator(str[i]))
+			word = 0;
+		else
+			word = 1;
+		i++;
+	}
+	return (str);
+}
