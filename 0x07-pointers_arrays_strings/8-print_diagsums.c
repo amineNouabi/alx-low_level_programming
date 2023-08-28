48369abafb47290033e9127d3a192cad8c3aa4bd
amineNouabi <aminenouabi9@gmail.com>
2023-08-28 13:40:39 -0700

    8 fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,40 +1,5 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_number - Prints an integer.
- * @n: Integer to print.
- * Return: void
- */
-void print_number(int n)
-{
-	unsigned int num;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -n;
-	}
-	else
-		num = n;
-
-	if (num / 10)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
-}
+#include <stdio.h>
 
 /**
  * print_diagsums - calculate sum of diags .
@@ -53,10 +18,6 @@ void print_diagsums(int *a, int size)
 		xdiag += a[i * size + size - 1 - i];
 		i++;
 	}
-	print_number(diag);
-	_putchar(',');
-	_putchar(' ');
-	print_number(xdiag);
-	_putchar('\n');
+	printf("%d, %d\n", diag, xdiag);
 }
 
