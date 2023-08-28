2b5fd4af416898fb87775a46a254f9ed8f8c876a
amineNouabi <aminenouabi9@gmail.com>
2023-08-28 09:48:05 -0700

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset -  fills memory with a constant byte.
+ * @s: memory block.
+ * @b: Character
+ * @n: number of bytes to fill.
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+		*(s + i++) = b;
+
+	return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+char *_memset(char *, char, unsigned int);
+
+#endif /* _MAIN_H_ */
