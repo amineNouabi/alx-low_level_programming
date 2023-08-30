90fe1a8b0b35cf86f13ed9aefde0dba1791e30bf
amineNouabi <aminenouabi9@gmail.com>
2023-08-30 14:10:32 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _sqrt - calculates square root of n.
+ * @n: number
+ * @a: iterator  from  0 to square root of n.
+ *
+ * Return: square root of n if n is a perfect square.
+ */
+int _sqrt(int n, int a)
+{
+	int square = a * a;
+
+	if (square == n)
+		return a;
+	else if (square > n)
+		return -1;
+	return _sqrt(n, a + 1);
+}
+
+
+/**
  * _sqrt_recursion - computes square root of an integer
  * @n: integer
  *
@@ -8,16 +27,7 @@
  */
 int _sqrt_recursion(int n)
 {
-	const int num = n;
-	int pow = n * n;
-
 	if (n < 0)
 		return (-1);
-
-	if (pow == num)
-		return (pow);
-	else if (pow < num)
-		return (-1);
-
-	return (_sqrt_recursion(n - 1));
+	return _sqrt(n, 0);
 }
