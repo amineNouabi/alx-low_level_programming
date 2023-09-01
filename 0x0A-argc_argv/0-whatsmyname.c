6702831c3c4e7431c30791b8264008fbb65caa15
amineNouabi <aminenouabi9@gmail.com>
2023-09-01 05:17:04 -0700

    Whats my name"

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the program.
+ * @argc: counr of args.
+ * @argv:  arguments.
+ *
+ * Return: (1) Always.
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
