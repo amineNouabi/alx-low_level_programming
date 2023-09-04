ea6614d4891b1bb7a94156d92e4f88cfb554c8b2
amineNouabi <aminenouabi9@gmail.com>
2023-09-04 17:37:47 +0100

    malloc-0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,14 +15,13 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (0);
 
-	arr = malloc(sizeof(char) * size + 1);
+	arr = malloc(sizeof(char) * size);
 
 	if (!arr)
 		return (0);
 
 	for (i = 0; i < size; i++)
 		arr[i] = c;
-	arr[i] = '\0';
 
 	return (arr);
 }
