d976b24a2881ab0a9bfcd6073a14133c03177f03
amineNouabi <aminenouabi9@gmail.com>
2023-09-12 01:33:31 +0100

    typed def dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,17 @@
  *
  * Description: The attributes of a dog.
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
 
+/**
+ * dog_t - alias for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
