5cc1e4b044f75bc9ef0c285783929877db05c2bb
amineNouabi <aminenouabi9@gmail.com>
2023-09-13 14:02:29 +0100

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+Pointers && Functions in C
