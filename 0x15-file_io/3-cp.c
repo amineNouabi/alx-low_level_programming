24b2360279ab47f9b1dec92c0d588781a9d7755e
amineNouabi <aminenouabi9@gmail.com>
2023-10-02 16:49:38 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,11 +11,14 @@
  * @argv: main arguments.
  * @status: int
  * @fd: file descriptor in case error 100 otherwise -1.
+ * @buffer: buffer.
  *
  * Return :void
  */
-void error_handler(char **argv, int status, int fd)
+void error_handler(char **argv, int status, int fd, char *buffer)
 {
+	if (buffer)
+		free(buffer);
 	if (status == 97)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 	else if (status == 98)
@@ -39,10 +42,10 @@ int main(int argc, char **argv)
 {
 	ssize_t count;
 	int fd_from, fd_to;
-	char *buffer;
+	char *buffer = 0;
 
 	if (argc != 3)
-		error_handler(argv, 97, -1);
+		error_handler(argv, 97, -1, buffer);
 
 	buffer = malloc(sizeof(char) * BUFFER_SIZE);
 	if (!buffer)
@@ -50,21 +53,22 @@ int main(int argc, char **argv)
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from < 0)
-		error_handler(argv, 98, -1);
+		error_handler(argv, 98, -1, buffer);
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to < 0)
-		error_handler(argv, 99, -1);
+		error_handler(argv, 99, -1, buffer);
 
 	while ((count = read(fd_from, buffer, BUFFER_SIZE)))
 		if (write(fd_to, buffer, count) != count)
-			error_handler(argv, 99, -1);
+			error_handler(argv, 99, -1, buffer);
 
 	if (count < 0)
-		error_handler(argv, 98, -1);
+		error_handler(argv, 98, -1, buffer);
 	if (close(fd_from) < 0)
-		error_handler(argv, 100, fd_from);
+		error_handler(argv, 100, fd_from, buffer);
 	if (close(fd_to) < 0)
-		error_handler(argv, 100, fd_to);
+		error_handler(argv, 100, fd_to, buffer);
+	free(buffer);
 	return (0);
 }
