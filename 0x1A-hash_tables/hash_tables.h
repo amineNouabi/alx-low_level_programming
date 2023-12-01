d50b8659ecfe6ff29a36c524b934bcf34411d615
amineNouabi <aminenouabi9@gmail.com>
2023-12-01 02:01:36 +0100

    key_index

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,5 +40,6 @@ typedef struct hash_table_s
 /* Functions prototypes */
 hash_table_t *hash_table_create(unsigned long int);
 unsigned long int hash_djb2(const unsigned char *);
+unsigned long int key_index(const unsigned char *, unsigned long int);
 
 #endif /* HASH_TABLES_H */
