972bd2bd6c49d913fc97cba87e83e934149dd638
amineNouabi <aminenouabi9@gmail.com>
2023-12-01 03:01:06 +0100

    hash table get

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	cursor = *(ht->array + index);
 
-
 	while (cursor)
 	{
 		if (!strcmp(cursor->key, key))
