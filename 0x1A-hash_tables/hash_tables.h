0a0d952d923ca95037dfb349722974b548c86cd5
amineNouabi <aminenouabi9@gmail.com>
2024-02-02 18:01:03 +0100

    Hash Table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -86,7 +86,7 @@ void hash_table_delete(hash_table_t *ht);
 shash_table_t *shash_table_create(unsigned long int size);
 shash_node_t *create_shash_node(const char *key, const char *value);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-void sht_insert_node(shash_table_t *sht, shash_node_t * node);
+void sht_insert_node(shash_table_t *sht, shash_node_t *node);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
