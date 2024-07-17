1ef9b074d7b51e1cb002a293b5bd54df26c0a2da
amineNouabi <aminenouabi9@gmail.com>
2024-07-17 20:24:22 +0100

    Jump algo in lists

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,64 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * jump_list - searches for a value
+ *        in a singly linked list of integers using jump search
+ * @list: pointer to the head of the list to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: the first address where value is located,
+ *     or NULL if value is not present or list is NULL
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t i, jump, j;
+	listint_t *cursor;
+
+	if (!list)
+		return (0);
+
+	jump = sqrt(size);
+	i = jump;
+	jump_to_index(cursor = list, i);
+
+	while (1)
+	{
+		if (cursor)
+			printf("Value checked at index [%ld] = [%d]\n", cursor->index, cursor->n);
+
+		if (!cursor || cursor->n >= value)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n", i - jump, i);
+			break;
+		}
+
+		i += jump;
+		jump_to_index(cursor, i);
+	}
+
+	j = i - jump;
+	jump_to_index(cursor = list, j);
+
+	while (cursor && cursor->index <= i)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", cursor->index, cursor->n);
+		if (cursor->n == value)
+			return (cursor);
+		cursor = cursor->next;
+	}
+	return (0);
+}
+
+/**
+ * jump_to_index - jumps to the node that has @index as index
+ * @cursor: address of pointer to shift.
+ * @index: target node's index
+ * Return: void.
+ */
+void jump_to_index(listint_t **cursor, size_t index)
+{
+	while (*cursor && (*cursor)->index < index)
+		(*cursor) = (*cursor)->next;
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,13 +3,36 @@
 
 #include <stddef.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 
 /** Utility Functions */
 void print_array(int *array, size_t left, size_t right);
+
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
+listint_t *create_list(int *array, size_t size);
+void jump_to_index(listint_t **cursor, size_t index);
 
 #endif /* SEARCH_ALGOS_H */
